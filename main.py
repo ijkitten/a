ada12d7680b4072186e6d309bd7974ca18ee561b
STU-ISAIAHISAIA <>
2022-02-08 14:39:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print("This is  a small game ")
+print("1:Play Game")
+selection=input("Select an option")
+if selection=="1":
+  print("You wake up in a dark room")
+  print("What do you do?")
+  print("1: Find a  flashlight")
+  selection=input("select an option")
+  if selection=="1":
+    print("You find a flashlight")
+    print("What do you do")
+    print("1: turn it on")
+    selection=input("select an option")
+    if selection=="1" :
+      print("You turn on thr flashlight")
+      print("you see that you are in the school gym,but dont remember going here")
+      print("What do you do?")
+      print("1: Scream")
+      print("2:get up")
+      selection=input("select an option")
+      if selection=="1":
+        print("You scream and people rush in.")
+        print("what do you do?")
+        print("1:Tell them what happened")
+               
