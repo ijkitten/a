b6502c85a50d10274c950d6d8789ed9fa828db72
STU ISAIAH ISAIAH <99269541+ijkitten@users.noreply.github.com>
2022-02-08 17:02:33 +0000

    Text based game

--- main.py
+++ main.py
@@ -22,4 +22,32 @@ if selection=="1":
         print("You scream and people rush in.")
         print("what do you do?")
         print("1:Tell them what happened")
+        print("2:Run away")
+        print("3 call your mom")
+        selection=input("Select an option")
+        if selection=="1":
+          print("the people morph into hideos montors and eat you")
+          exit(print("GAME OVER"))
+        if selection=="3":
+          
+          print("you call your mom and she tells you to dial 911 and run")
+          print("what do you do?")
+          print("1:Run and dial 911")
+          print("2:Run")
+          print("3:Dial 911")
+          print("4:do nothing")
+          selection=input("Select an option")
+          if selection=="1":
+            print("you escape and see a old ford truck nearby")
+            print("But the police come and morph into monsters then they eat you")
+          
+        if selection=="2":
+          print("You run away and escape")
+          print("You see an old rusty ford truck nearby")
+          print("What do you do?")
+          print("1:Run to the ford")
+          print("1:run further")
+          selection=input("Select an option")
+          
+
                
